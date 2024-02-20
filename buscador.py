6cf6ddc107d02bc70c071725905c271c2a6d091a
Geraldo Júnior <geraldo.gj63@gmail.com>
2024-02-20 12:06:01 -0300

    Adicionando laço de repetição + função para finalizar ou continuar a pesquisa

--- buscador.py
+++ buscador.py
@@ -1,10 +1,27 @@
 # O intuito deste código é para realizar a busca de um item em uma lista e informar se o mesmo esta presente ou não. 
 
-corCliente = input('Informe a cor desejada: ')
-
 corEstoque = ['azul', 'amarelo', 'verde', 'roxo', 'cinza']
 
-if corCliente.lower() in corEstoque:
-    print('A cor esta disponível no estoque.')
-else: 
-    print('A cor não esta disponível no estoque.')
+def loop():
+    global continuar # Reconhece a variavel continuar que esta fora da função
+
+    resultadoLoop = input('\nDeseja pesquisar mais outra cor? Y/N \n')
+
+    if resultadoLoop.lower() == 'y' or resultadoLoop.lower() == 'yes':
+        continuar = True
+    else: 
+        continuar = False
+
+
+continuar = True
+
+while continuar is True:
+    corCliente = input('Informe a cor desejada: ')
+
+    if corCliente.lower() in corEstoque:
+        print('\nA cor esta disponível no estoque.')
+
+    else: 
+        print('\nA cor não esta disponível no estoque.')
+    
+    loop()
